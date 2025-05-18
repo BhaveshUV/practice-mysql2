c9874e2dccb1ae551bfcb5c4c34b1b6bb5cf7883
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2025-05-19 05:22:57 +0530

    form & test connection w mysql-srv using mysql pkg
    - create .env file and store mysql server password
    - install dotenv pkg for loading .env variables into the process.env obj

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'app',
+  password: process.env.PASSWORD
+});
+
+try {
+    connection.query("SHOW TABLES", (err, results) => { //Asynchronous (Doesn't block the execution)
+        if(err) throw err;
+        console.log(results);
+    });
+} catch (err) {
+    console.log(err);
+}
+
+connection.end();
 
 let createRandomUser = () => {
   return {
