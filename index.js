cce1543b1084927c004340476ad6cbcb5ecafeb8
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2025-05-20 14:44:53 +0530

    manually bulk insert multiple rows in 'user' table
    - to insert rows—use bulk insertion format i.e. VALUES ?
    - add query.sql file to create the 'user' table using MySQL CLI via
      'source' command

--- index.js
+++ index.js
@@ -10,13 +10,26 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD
 });
 
+// let query = `SHOW TABLES`;
+let query = "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES ?"; // Here, ? expects bulk insertion format i.e [[[], [], []]]
+let users = [
+  ['a', 'abc', 'abc@gmail.com', 'abc@123'],
+  ['b', 'bcd', 'bcd@gmail.com', 'bcd@123']
+];
+
 try {
-    connection.query("SHOW TABLES", (err, results) => { //Asynchronous (Doesn't block the execution)
-        if(err) throw err;
-        console.log(results);
-    });
+  connection.query(query, [users], (err, results) => { //Asynchronous (Doesn't block the execution)
+    if (err) throw err;
+    console.log(results);
+    if (results.length > 0) {
+      console.log(`Total results = ${results.length}`)
+      for (let result of results) {
+        console.log(result);
+      }
+    }
+  });
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
 
 connection.end();
