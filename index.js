045ccb1cdd7eeff396e8c34b5c53143efa707648
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2025-05-21 23:41:51 +0530

    build express srv—user endpts, mysql conn, ejs vws
    - setup express server with EJS view engine
    - configure static files (/public), views, and middleware (urlencoded, method-override)
    - add style.css in /public for ejs pages
      - home pg (total number of users)
      - all users pg (cards & tabular format)
      - edit user pg with form
    - add endpoints to:
      - get total number of users
      - get all users (cards & tabular format)
      - update username with password verification
    - remove connection.end() to keep MySQL connection active for dynamic requests

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+
+//----------------------------faker pkg----------------------------//
 
 let createRandomUser = () => {
   return [
@@ -11,6 +16,8 @@ let createRandomUser = () => {
   ];
 }
 
+//----------------------------mysql2 pkg----------------------------//
+
 dotenv.config();
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -19,26 +26,95 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD
 });
 
-// let query = `SHOW TABLES`;
-let query = "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES ?"; // Here, ? expects bulk insertion format i.e [[[], [], []]]
-let data = [];
-for(let i=1; i<=100; i++) {
-  data.push(createRandomUser());
-}
+//--------------------------express.js srv--------------------------//
 
-try {
-  connection.query(query, [data], (err, results) => { //Asynchronous (Doesn't block the execution)
-    if (err) throw err;
-    console.log(results);
-    if (results.length > 0) {
-      console.log(`Total results = ${results.length}`)
-      for (let result of results) {
-        console.log(result);
+const server = express();
+server.listen(8080, () => {
+  console.log("The server has started listening for requests...");
+});
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "/views"));
+
+// For all requests:
+// use static files(for stylesheets, js, images) from /public
+server.use(express.static(path.join(__dirname, "/public")));
+// use middlewares(for processing request before route handlers to parse it)
+// middleware—parse URL-encoded form data (from HTML forms)
+server.use(express.urlencoded({ extended: true }));
+server.use(methodOverride("_method"));
+
+// GET — Home Route
+server.get("/", (req, res) => {
+  try {
+    connection.query("SELECT COUNT(DISTINCT id) FROM user WHERE id IS NOT NULL", (err, results) => {
+      if (err) throw err;
+      let totalUsers = results[0]["COUNT(DISTINCT id)"];
+      console.log(results, totalUsers);
+      res.render("home.ejs", { totalUsers });
+    });
+  } catch (err) {
+    res.send("Error: " + err);
+  }
+});
+
+// GET — All Users Route (Cards form)
+server.get("/users", (req, res) => {
+  try {
+    connection.query("SELECT id, username, email FROM user", (err, results) => {
+      if (err) throw err;
+      // results –> array of objects/rows
+      res.render("users.ejs", { users: results });
+    });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// GET — All Users Route (Tabular form)
+server.get("/users-tabular", (req, res) => {
+  try {
+    connection.query("SELECT id, username, email FROM user", (err, results) => {
+      if (err) throw err;
+      // results –> array of objects/rows
+      res.render("usersTabular.ejs", { users: results });
+    });
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+// GET — Edit Username Route
+server.get("/users/:id", (req, res) => {
+  console.log("get request triggered...")
+  let { id } = req.params;
+  console.log(id);
+  try {
+    connection.query("SELECT * FROM user WHERE id = ?", [[`${id}`]], (err, result) => {
+      if (err) throw err;
+      console.log("Form to edit user --> ", result);
+      res.render("editUser.ejs", { user: result[0] });
+    });
+  } catch (err) {
+    res.send("Error: " + err);
+  }
+});
+
+// UPDATE — Patch/update Username Route
+server.patch("/users/:id", (req, res) => {
+  console.log("req.body --> ", req.body);
+  try {
+    connection.query("SELECT password FROM user WHERE id = ?", [req.params.id], (err, result) => {
+      if (err) throw err;
+      else if (req.body.password != result[0].password) res.send("Incorrect Password!");
+      else {
+        connection.query("UPDATE user SET username = ? WHERE id = ? AND password = ?", [req.body.username, req.params.id, req.body.password], (err, result) => {
+          if (err) throw err;
+          console.log(result);
+          res.redirect("/users");
+        });
       }
-    }
-  });
-} catch (err) {
-  console.log(err);
-}
-
-connection.end();
+    });
+  } catch (err) {
+    res.send("Error: " + err);
+  }
+});
